98ab1bcae2ac41f33d0a5d9494b17ae4d4bd3f5b
Latasha Simmons <lsimmons1832@gmail.com>
2017-03-07 19:57:11 -0600

    add screenshot

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 console.log("First line in JS file: ", Date.now());
+var treeContainer = document.getElementById("treeContainer");
 
+var treehouseString = "";
+var myBadge; 
+function makeDOM(xhrData){
+	for (var i = 0; i < xhrData.badges.length; i++) {
+		myBadge = xhrData.badges[i];
+
+		treehouseString += `<div class="col-sm-6 col-md-4 cards">`;
+		treehouseString += `<h4>${myBadge.name}</h4>`;
+		treehouseString += `<img src="${myBadge.icon_url}" alt="" class="img-thumbnail img-responsive">`;
+		treehouseString += `</div>`;
+	}
+		console.log(myBadge);
+		treeContainer.innerHTML += treehouseString;
+
+}
 
 
 function executeThisCodeAfterFileLoaded() {
 	console.log("Data Returned: ", Date.now());
 	var data = JSON.parse(this.responseText);
 	console.log("My Treehouse: ", data);
+	makeDOM(data);
 }
 
 function executeThisCodeAfterFileFails() {
